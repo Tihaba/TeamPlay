9095b29fc4bade0462aa10dd43a7352d75229649
minseo Gang <neoclick04@gmail.com>
2024-05-19 22:29:33 +0900

    수정본

--- heap/Main.java
+++ heap/Main.java
@@ -6,7 +6,6 @@ public class Main {
 	private int heap[];//배열
 	private int N;//원소(노드)의 갯수
 	private int result[];//삭제한 원소배열
-	int num=0;//result 인덱스
 	int zero=0;//출력갯수
 	public Main(int size) {
 		heap= new int[size+1];//0이 아니라 1부터 채우기 위함
@@ -14,7 +13,7 @@ public class Main {
 		N=0;//원소(노드)의 갯수
 	}
 	public void insert(int x) {
-		if(x==0) {result[++num]=delete(); zero++;}//0을 만나면 삭제
+		if(x==0) result[++zero]=delete();//0을 만나면 삭제
 		else {
 			N++;//원소(노드) 갯수 선증가
 			heap[N]=x;//삽입(우선순위 큐 이므로 마지막에 삽입)
@@ -31,7 +30,7 @@ public class Main {
 			percolate_up(parent);//재귀호출로 완전히 배치될때까지
 		}
 	}
-	
+
 	public Integer delete() {//삭제후 반환
 		if(isEmpty()) return 0;
 		else {
@@ -45,28 +44,23 @@ public class Main {
 	public void percolate_down(int i) {//위에서 아래로 힙구조로 재배치
 
 	}
-	
 	public boolean isEmpty() {//비었는지 true false
 		return N==0;
 	}
-
-	
 	public void Print() {//출력부
 		for(int i=1; i<=zero; i++)
 			System.out.println(result[i]);
 	}
-	
 	public static void main(String[] args) {
 		Scanner s= new Scanner(System.in);
 		int N=s.nextInt();//입력개수 받음
-		int S;
-		Main a= new Main(N);
-		for(int i=1; i<=N; i++) {
+		int S;//입력값
+		Main a= new Main(N);//힙생성
+		for(int i=1; i<=N; i++) {//입력
 			S=s.nextInt();
 			a.insert(S);
 		}
 		a.Print();
 		s.close();
 	}
-
 }
